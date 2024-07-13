b513f7c9177f4fe4659d37ef183dc346f931810e
Mateusz <101966939+Rozoom7721@users.noreply.github.com>
2024-07-13 15:08:54 +0200

    Add documentation and change some name

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -10,7 +10,7 @@ namespace FilmTicketShop.Models
 		[Key]
 		public int Id { get; set; }
 
-		public string Title { get; set; }
+		public string? Title { get; set; }
 
 		public string? Description { get; set; }
 
